d79d18db26722aefccff29f54155a8f666a69c1b
cooolgamer <64099608+cooolgamer@users.noreply.github.com>
2024-09-28 15:06:53 +0200

    fixed a commit that I didn't merge

--- sysmodules/rosalina/source/menu.c
+++ sysmodules/rosalina/source/menu.c
@@ -253,10 +253,8 @@ static Result menuUpdateMcuInfo(void)
         mcuFwVersion = SYSTEM_VERSION(major - 0x10, minor, 0);
     }
 
-    // https://www.3dbrew.org/wiki/I2C_Registers#Device_3
-    MCUHWC_ReadRegister(0x58, dspVolumeSlider, 2); // Register-mapped ADC register
-    MCUHWC_ReadRegister(0x27, volumeSlider + 0, 1); // Raw volume slider state
-    MCUHWC_ReadRegister(0x09, volumeSlider + 1, 1); // Volume slider state
+    if (!mcuInfoTableRead)
+        mcuInfoTableRead = R_SUCCEEDED(MCUHWC_ReadRegister(0x7F, mcuInfoTable, sizeof(mcuInfoTable)));
 
     svcCloseHandle(*mcuHwcHandlePtr);
     return res;
